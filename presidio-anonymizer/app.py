800f89e36efee629e206de5c67c427945fc119c2
Victor <theowlvic@users.noreply.github.com>
2025-12-08 16:29:18 +0000

    Removed unused import

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 from flask import Flask, Response, jsonify, request
 from presidio_anonymizer import AnonymizerEngine, DeanonymizeEngine
-from presidio_anonymizer.entities import InvalidParamError, OperatorConfig
+from presidio_anonymizer.entities import InvalidParamError
 from presidio_anonymizer.services.app_entities_convertor import AppEntitiesConvertor
 from werkzeug.exceptions import BadRequest, HTTPException
 
